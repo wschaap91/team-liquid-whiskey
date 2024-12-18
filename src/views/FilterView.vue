8efaca9d23f0744eabfa392339f23037b903de83
Wouter Schaap <hallo@koffiemetschaap.nl>
2024-12-18 16:57:38 +0100

    Dit is nieuwe code

--- src/views/FilterView.vue
+++ src/views/FilterView.vue
@@ -1,39 +1,39 @@
 <script setup>
 import { ref, computed } from 'vue'
-import WhiskeyList from '../components/WhiskeyList.vue';
-import RegionFilter from '../components/RegionFilter.vue';
-import { unique } from '../assets/js/utilities.js';
+import WhiskeyList from '../components/WhiskeyList.vue'
+import RegionFilter from '../components/RegionFilter.vue'
+import { unique } from '../assets/js/utilities.js'
 
 const props = defineProps({
-    whiskies: {
-        type: Array,
-        required: true
-    }
+  whiskies: {
+    type: Array,
+    required: true
+  }
 })
 
 const regions = computed(() => {
-    const defaultArray = ['all'];
+  const defaultArray = ['all']
 
-    if (!props.whiskies.length) {
-        return defaultArray;
-    }
+  if (!props.whiskies.length) {
+    return defaultArray
+  }
 
-    const regions = [...defaultArray, ...props.whiskies.map(w => w.region)];
-    return regions.filter(unique);
+  const regions = [...defaultArray, ...props.whiskies.map((w) => w.region)]
+  return regions.filter(unique)
 })
 
-const activeFilter = ref('all');
+const activeFilter = ref('all')
 
 const setActiveFilter = (region = 'all') => {
-    activeFilter.value = region;
+  activeFilter.value = region
 }
+
+//Dit is een testje
 </script>
 
 <template>
-    <h1 class="page-title o-title">Whiskey Selection</h1>
+  <h1 class="page-title o-title">Whiskey Selection</h1>
 
-    <RegionFilter :regions="regions"
-                  @change-region="setActiveFilter" />
-    <WhiskeyList :whiskies="props.whiskies"
-                 :activeRegion="activeFilter" />
+  <RegionFilter :regions="regions" @change-region="setActiveFilter" />
+  <WhiskeyList :whiskies="props.whiskies" :activeRegion="activeFilter" />
 </template>
